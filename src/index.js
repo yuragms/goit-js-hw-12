938e11d0228ae9ae5ffff8e306321f1143bd65dd
yuragms <yura.gms@gmail.com>
2021-07-19 10:09:00 +0300

    5

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ const DEBOUNCE_DELAY = 300;
 
 const countryArr = fetch ('https://restcountries.eu/rest/v2/name/uk')
 .then(response => { return response.json();})
-.then( country => {console.log(country);})
+.then( countries => {
+    console.log(countries);
+    countryInfo.innerHTML = countyTemplates(countries[0]);
+})
 .catch( error => {console.log('error');});
 
 console.log(countryArr);
 
-countryInfo.innerHTML = countyTemplates;
+countryInfo.innerHTML = countyTemplates();
 
